82355af0d8f78e98b2d342fc16081244763da0dc
Sera_Is <isonserena@hotmail.it>
2022-04-01 19:45:26 +0200

    ex 5 extra credit - compose styles and classes in the Box component

--- src/exercise/05.js
+++ src/exercise/05.js
@@ -4,13 +4,14 @@
 import * as React from 'react'
 import '../box-styles.css'
 
-function Box({className = '', style, ...otherProps}) { 
-return <div className={`box ${className}`.trim()} style={{fontStyle: 'italic', ...style}} {...otherProps} />
+function Box({style, size, className = '', ...otherProps}) {  
+const sizeClassName = size ? `box--${size}` : '';
+return <div className={`box ${sizeClassName}`.trim()} style={{fontStyle: 'italic', ...style}} {...otherProps} />
 }
 
-const smallBox = <Box className="box--small" style={{backgroundColor: 'lightblue'}}>small lightblue box</Box>
-const mediumBox = <Box className="box--medium" style={{backgroundColor: 'pink'}}>medium pink box</Box>
-const largeBox = <Box className="box--large" style={{backgroundColor: 'orange'}}>large orange box</Box>
+const smallBox = <Box size="small" style={{backgroundColor: 'lightblue'}}>small lightblue box</Box>
+const mediumBox = <Box size="medium" style={{backgroundColor: 'pink'}}>medium pink box</Box>
+const largeBox = <Box size="large" style={{backgroundColor: 'orange'}}>large orange box</Box>
 
 function App() {
   return (
